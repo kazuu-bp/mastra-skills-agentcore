d28dc88d5dfd8df19107b0e05a2522ef7dbbf07f
kazuu-bp <ksponpon.0215@gmail.com>
2026-03-03 21:31:53 +0900

    出力の調整中

--- agent/src/mastra/workspaces/skills-wrokspace.ts
+++ agent/src/mastra/workspaces/skills-wrokspace.ts
@@ -3,7 +3,7 @@ import { Workspace, LocalFilesystem, LocalSandbox } from '@mastra/core/workspace
 export const skillsWorkspace = new Workspace({
   filesystem: new LocalFilesystem({
     basePath: './workspace',
-    instructions: '生成したファイルを出力する場合は、必ず "/outputs/" ディレクトリに保存してください。',
+    instructions: '生成したファイルを出力する場合は、必ず "outputs/" ディレクトリに保存してください。',
   }),
   sandbox: new LocalSandbox({
     workingDirectory: './workspace',
